1f507c6793eb77cd8ac1cc4c22f920443592f5f2
ku18ch <164178948+ku18ch@users.noreply.github.com>
2024-06-04 20:05:32 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+    /*=================================================
+    スムーススクロール
+    ===================================================*/
+    $('a[href^="#"]').click(function () {
+        let href = $(this).attr("href");
+        let target = $(href == "#" || href == "" ? "html" : href);
+        let position = target.offset().top;
+      // animateでスムーススクロールを行う   ページトップからpositionだけスクロールする
+      // 600はスクロール速度で単位はミリ秒  swingはイージングのひとつ
+        $("html, body").animate({ scrollTop: position }, 2000, "swing");
+        return false;
+    });
+});
